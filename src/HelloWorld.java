6703360f240628ca7b42e90963238c149a0a6bd1
luojun853 <as853472853@gmail.com>
2021-09-23 21:48:07 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello!ZYH");
+        System.out.println("Hellaaao!ZYH");
         System.out.println("Hello!GJF");
     }
 }
